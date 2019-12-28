4e118c19bf069e4ceb1b28eecc0242402f799ae1
Tien Pham <tiendev94@gmail.com>
2019-12-28 15:11:27 +0700

    let const

---
+++ 3-letconst.js
@@ -0,0 +1,7 @@
+// function vs block scope
+// let scope to block
+// var scope to function
+
+// inside closure
+// var: creates 1 variable, closure latches on
+// let creates a new variable for each iteration
